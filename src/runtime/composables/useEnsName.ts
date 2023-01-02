84cd8e830cda3a8d3d0961eb572587c55e092a26
zaifer <zaif3r@pm.me>
2023-01-02 18:37:20 -0500

    fix: typo

--- src/runtime/composables/useEnsName.ts
+++ src/runtime/composables/useEnsName.ts
@@ -1,6 +1,6 @@
 import { useEnsName as useVagmiEnsName } from "vagmi";
 
-export function useEnsAddress(options: Parameters<typeof useVagmiEnsName>[0]) {
+export function useEnsName(options: Parameters<typeof useVagmiEnsName>[0]) {
   const ensName = useVagmiEnsName(options);
 
   return ensName;
