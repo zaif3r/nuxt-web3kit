e47e87b8ad43756f2a1064197dddc2913b260d50
zaifer <zaif3r@pm.me>
2023-01-02 19:59:15 -0500

    simplify connect logic

--- src/runtime/composables/useConnect.ts
+++ src/runtime/composables/useConnect.ts
@@ -1,25 +1,15 @@
 import { useRuntimeConfig } from "#app";
-import { watch, computed } from "vue";
-import {
-  useAccount as useVagmiAccount,
-  useConnect as useVagmiConnect,
-  useDisconnect as useVagmiDisconnect,
-} from "vagmi";
-import { useAccountState } from "./useAccountState";
+import { useConnect as useVagmiConnect } from "vagmi";
 import { useConnectionCookie } from "./useConnectionCookie";
 
 export function useConnect(connectArgs: Parameters<typeof useVagmiConnect>[0]) {
-  const {
-    web3kit: { vagmi: config },
-  } = useRuntimeConfig().public;
+  const { web3kit: { vagmi: config } } = useRuntimeConfig().public;
 
-  const vagmiAccount = useVagmiAccount();
-  const vagmiDisconnect = useVagmiDisconnect();
+  const connectionCookie = useConnectionCookie();
+
   const vagmiConnect = useVagmiConnect({
     ...connectArgs,
     onConnect: (result, args, context) => {
-      setAccount(vagmiAccount);
-
       if (config.autoConnect) {
         connectionCookie.value = {
           isConnected: true,
@@ -30,40 +20,5 @@ export function useConnect(connectArgs: Parameters<typeof useVagmiConnect>[0]) {
     },
   });
 
-  const { setState: setAccount } = useAccountState();
-  const connectionCookie = useConnectionCookie();
-
-  async function connect() {
-    await vagmiConnect.connectAsync.value();
-  }
-
-  async function disconnect() {
-    await vagmiDisconnect.disconnectAsync();
-  }
-
-  watch(vagmiAccount.status, (status) => {
-    if (status == "connected") {
-      setAccount(vagmiAccount);
-    } else if (status == "disconnected") {
-      setAccount(null);
-      connectionCookie.value = {
-        isConnected: false,
-      };
-    }
-  });
-
-  const isConnected = computed(() => {
-    if (config.autoConnect) {
-      return (
-        vagmiAccount.isConnected.value || !!connectionCookie.value?.isConnected
-      );
-    }
-    return vagmiAccount.isConnected.value;
-  });
-
-  return {
-    connect,
-    disconnect,
-    isConnected,
-  };
+  return vagmiConnect;
 }
