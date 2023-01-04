19c9d3f6f14a247bb7b52a62f203d753ccb8f7e1
zaifer <zaif3r@pm.me>
2023-01-04 16:46:17 -0500

    feat: add useEnsAvatar composable

--- src/runtime/composables/useEns.ts
+++ src/runtime/composables/useEns.ts
@@ -1,7 +1,9 @@
 import {
   useEnsName as useVagmiEnsName,
   useEnsAddress as useVagmiEnsAddress,
+  useEnsAvatar as useVagmiEnsAvatar,
 } from "vagmi";
 
 export const useEnsName = useVagmiEnsName;
 export const useEnsAddress = useVagmiEnsAddress;
+export const useEnsAvatar = useVagmiEnsAvatar;
